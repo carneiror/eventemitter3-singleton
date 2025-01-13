cb6bc426c2881b12981c8425bcd06859ad921f30
Rui Carneiro <rui.carneiro@paddypowerbetfair.com>
2025-01-13 14:33:03 +0000

    add ts support

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// Import the EventEmitter3 types
+import { EventEmitter } from "eventemitter3";
+
+/**
+ * A registry instance powered by EventEmitter3.
+ * You can use this to subscribe to and emit events.
+ */
+declare const registry: EventEmitter;
+
+export default registry;
